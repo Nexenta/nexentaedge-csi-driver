fddcd8dbfe74ed2df73e21f675f78918347876fc
sabbot <sabbotagge@gmail.com>
2018-04-06 18:51:51 +0300

    NED-5962 fix tabsize to 8

--- csi/nexentaedge/nexentaedge.go
+++ csi/nexentaedge/nexentaedge.go
@@ -1,12 +1,13 @@
 package nexentaedge
 
 import (
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "sync"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"sync"
 
-    log "github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 )
 
 const defaultChunkSize int = 1048576
@@ -14,33 +15,33 @@ const defaultMountPoint string = "/var/lib/ndnfs"
 
 /*INexentaEdge interface to provide base methods */
 type INexentaEdge interface {
-    CreateVolume(name string, size int) (string, error)
-    DeleteVolume(name string) error
-    ListVolumes() (map[string]string, error)
-    //AttachVolume(instanceID, volumeID string) (string, error)
-    //WaitDiskAttached(instanceID string, volumeID string) error
-    //DetachVolume(instanceID, volumeID string) error
-    //WaitDiskDetached(instanceID string, volumeID string) error
+	CreateVolume(name string, size int) (string, error)
+	DeleteVolume(name string) error
+	ListVolumes() (map[string]string, error)
+	//AttachVolume(instanceID, volumeID string) (string, error)
+	//WaitDiskAttached(instanceID string, volumeID string) error
+	//DetachVolume(instanceID, volumeID string) error
+	//WaitDiskDetached(instanceID string, volumeID string) error
 }
 
 type Config struct {
-    Name        string
-    Nedgerest   string
-    Nedgeport   int16
-    Nedgedata   string
-    Clustername string
-    Tenantname  string
-    Chunksize   int
-    Username    string
-    Password    string
-    Mountpoint  string
-    Servicename string
+	Name        string
+	Nedgerest   string
+	Nedgeport   int16
+	Nedgedata   string
+	Clustername string
+	Tenantname  string
+	Chunksize   int
+	Username    string
+	Password    string
+	Mountpoint  string
+	Servicename string
 }
 
 type NexentaEdgeProvider struct {
-    Mutex    *sync.Mutex
-    Endpoint string
-    Config   *Config
+	Mutex    *sync.Mutex
+	Endpoint string
+	Config   *Config
 }
 
 var NexentaEdgeInstance INexentaEdge = nil
@@ -48,61 +49,87 @@ var configFile = "/etc/nexentaedge.json"
 
 /*InitNexentaEdgeProvider set up variables*/
 func InitNexentaEdgeProvider(config string) {
-    configFile = config
-    log.Infof("InitNexentaEdgeProvider configFile: %s", configFile)
+	configFile = config
+	log.Infof("InitNexentaEdgeProvider configFile: %s", configFile)
 }
 
 func ReadParseConfig(fname string) Config {
-    content, err := ioutil.ReadFile(fname)
-    if err != nil {
-        log.Fatal("Error reading config file: ", fname, " error: ", err)
-    }
-    var conf Config
-    err = json.Unmarshal(content, &conf)
-    if err != nil {
-        log.Fatal("Error parsing config file: ", fname, " error: ", err)
-    }
-    return conf
+	content, err := ioutil.ReadFile(fname)
+	if err != nil {
+		log.Fatal("Error reading config file: ", fname, " error: ", err)
+	}
+	var conf Config
+	err = json.Unmarshal(content, &conf)
+	if err != nil {
+		log.Fatal("Error parsing config file: ", fname, " error: ", err)
+	}
+	return conf
 }
 
 /*GetNexentaEdgeProvider returns nedge provider instance*/
 func GetNexentaEdgeProvider() (INexentaEdge, error) {
-    log.Info("GetNexentaedgeProvider: ")
-    if NexentaEdgeInstance == nil {
+	log.Info("GetNexentaedgeProvider: ")
+	if NexentaEdgeInstance == nil {
 
-        conf := ReadParseConfig(configFile)
-        if conf.Chunksize == 0 {
-            conf.Chunksize = defaultChunkSize
-        }
-        if conf.Mountpoint == "" {
-            conf.Mountpoint = defaultMountPoint
-        }
+		conf := ReadParseConfig(configFile)
+		if conf.Chunksize == 0 {
+			conf.Chunksize = defaultChunkSize
+		}
+		if conf.Mountpoint == "" {
+			conf.Mountpoint = defaultMountPoint
+		}
 
-        log.Info("GetNexentaedgeProvider config: ", conf)
-        NexentaEdgeInstance = &NexentaEdgeProvider{
-            Mutex:    &sync.Mutex{},
-            Endpoint: fmt.Sprintf("http://%s:%d/", conf.Nedgerest, conf.Nedgeport),
-            Config:   &conf,
-        }
-    }
+		log.Info("GetNexentaedgeProvider config: ", conf)
+		NexentaEdgeInstance = &NexentaEdgeProvider{
+			Mutex:    &sync.Mutex{},
+			Endpoint: fmt.Sprintf("http://%s:%d/", conf.Nedgerest, conf.Nedgeport),
+			Config:   &conf,
+		}
+	}
 
-    return NexentaEdgeInstance, nil
+	return NexentaEdgeInstance, nil
 }
 
 /*CreateVolume remotely creates bucket on nexentaedge service*/
 func (nedge *NexentaEdgeProvider) CreateVolume(name string, size int) (volume string, err error) {
-    log.Info("NexentaEdgeProvider:CreateVolume name : ", name, " size:", size)
-    return name, nil
+	log.Info("NexentaEdgeProvider:CreateVolume name : ", name, " size:", size)
+	return nedge.GetVolumeID(name), nil
 }
 
 /*DeleteVolume remotely deletes bucket on nexentaedge service*/
-func (nedge *NexentaEdgeProvider) DeleteVolume(name string) (err error) {
-    log.Info("NexentaEdgeProvider:DeleteVolume  name: ", name)
-    return nil
+func (nedge *NexentaEdgeProvider) DeleteVolume(volumeId string) (err error) {
+	log.Info("NexentaEdgeProvider:DeleteVolume  name: ", volumeId)
+	return nil
 }
 
 /*ListVolumes list all available volumes*/
 func (nedge *NexentaEdgeProvider) ListVolumes() (map[string]string, error) {
-    log.Info("NexentaEdgeProvider ListVolumes: ")
-    return nil, nil
+	log.Info("NexentaEdgeProvider ListVolumes: ")
+	return nil, nil
+}
+
+/* ToVolumeID converts service, cluster, tenant, bucket values to ID */
+func ToVolumeID(service string, cluster string, tenant string, bucket string) string {
+	return fmt.Sprintf("%s/%s/%s/%s", service, cluster, tenant, bucket)
+}
+
+func FromVolumeID(volumeId string) (resultMap map[string]string) {
+
+	parts := strings.Split(volumeId, "/")
+	resultMap["servicename"] = parts[0]
+	resultMap["clustername"] = parts[1]
+	resultMap["tenantname"] = parts[2]
+	resultMap["bucketname"] = parts[3]
+
+	return resultMap
+}
+
+func (nedge *NexentaEdgeProvider) GetVolumeID(bucketname string) string {
+
+	return ToVolumeID(
+		nedge.Config.Servicename,
+		nedge.Config.Clustername,
+		nedge.Config.Tenantname,
+		bucketname,
+	)
 }
--- csi/nodeserver.go
+++ csi/nodeserver.go
@@ -1,45 +1,66 @@
 package csi
 
 import (
-    "github.com/container-storage-interface/spec/lib/go/csi/v0"
-    csicommon "github.com/kubernetes-csi/drivers/pkg/csi-common"
-    log "github.com/sirupsen/logrus"
-    "golang.org/x/net/context"
+	"os"
+
+	"github.com/container-storage-interface/spec/lib/go/csi/v0"
+	csicommon "github.com/kubernetes-csi/drivers/pkg/csi-common"
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"k8s.io/kubernetes/pkg/util/mount"
 )
 
 type nodeServer struct {
-    *csicommon.DefaultNodeServer
+	*csicommon.DefaultNodeServer
 }
 
 func (ns *nodeServer) NodeGetId(ctx context.Context, req *csi.NodeGetIdRequest) (*csi.NodeGetIdResponse, error) {
-    log.Infof("NodeGetId req[%#v]", req)
-    // Using default function
-    log.Info("NodeGetId invoked")
-    return ns.DefaultNodeServer.NodeGetId(ctx, req)
+	log.Infof("NodeGetId req[%#v]", req)
+	// Using default function
+	log.Info("NodeGetId invoked")
+	return ns.DefaultNodeServer.NodeGetId(ctx, req)
 }
 
 func (ns *nodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePublishVolumeRequest) (*csi.NodePublishVolumeResponse, error) {
-    log.Infof("NodePublishVolume req[%#v]", req)
-    targetPath := req.GetTargetPath()
-    fsType := req.GetVolumeCapability().GetMount().GetFsType()
-    devicePath := req.GetPublishInfo()["DevicePath"]
+	log.Infof("NodePublishVolume req[%#v]", req)
+	targetPath := req.GetTargetPath()
+	fsType := req.GetVolumeCapability().GetMount().GetFsType()
+	devicePath := req.GetPublishInfo()["DevicePath"]
 
-    log.Info("NodePublishVolume invoked: targetPath:", targetPath, ", fsType:", fsType, " devicePath:", devicePath)
-    return &csi.NodePublishVolumeResponse{}, nil
+	notMnt, err := mount.New("").IsLikelyNotMountPoint(targetPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			if err := os.MkdirAll(targetPath, 0750); err != nil {
+				return nil, status.Error(codes.Internal, err.Error())
+			}
+			notMnt = true
+		} else {
+			return nil, status.Error(codes.Internal, err.Error())
+		}
+	}
+
+	if !notMnt {
+		return &csi.NodePublishVolumeResponse{}, nil
+	}
+
+	log.Info("NodePublishVolume invoked: targetPath:", targetPath, ", fsType:", fsType, " devicePath:", devicePath)
+	return &csi.NodePublishVolumeResponse{}, nil
 }
 
 func (ns *nodeServer) NodeUnpublishVolume(ctx context.Context, req *csi.NodeUnpublishVolumeRequest) (*csi.NodeUnpublishVolumeResponse, error) {
-    log.Infof("NodeUnpublishVolume req[%#v]", req)
-    targetPath := req.GetTargetPath()
-    log.Info("NodeUnpublishVolume invoked: targetPath:", targetPath)
+	log.Infof("NodeUnpublishVolume req[%#v]", req)
+	targetPath := req.GetTargetPath()
+	log.Info("NodeUnpublishVolume invoked: targetPath:", targetPath)
 
-    return &csi.NodeUnpublishVolumeResponse{}, nil
+	return &csi.NodeUnpublishVolumeResponse{}, nil
 }
 
 func (ns *nodeServer) NodeUnstageVolume(ctx context.Context, req *csi.NodeUnstageVolumeRequest) (*csi.NodeUnstageVolumeResponse, error) {
-    return &csi.NodeUnstageVolumeResponse{}, nil
+	return &csi.NodeUnstageVolumeResponse{}, nil
 }
 
 func (ns *nodeServer) NodeStageVolume(ctx context.Context, req *csi.NodeStageVolumeRequest) (*csi.NodeStageVolumeResponse, error) {
-    return &csi.NodeStageVolumeResponse{}, nil
+	return &csi.NodeStageVolumeResponse{}, nil
 }
